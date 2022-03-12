cd348bd39b220067abd65063e79cfc995738df84
Alexandre Pereira <lexandre32@gmail.com>
2022-03-11 23:20:29 -0300

    🌹🌹

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,15 +1,17 @@
 defmodule ListFilter do
   def call(list) do
-  list
-  |>Enum.map(fn x -> "#{x}" end)
-  |>Enum.flat_map(fn x -> parseInteger(x)end)
-  |>Enum.filter(fn x -> rem(x,2)!=0 end)
-
+    list
+    |> Enum.map(fn x -> "#{x}" end)
+    |> Enum.flat_map(fn x -> parseInteger(x) end)
+    |> Enum.filter(fn x -> rem(x, 2) != 0 end)
   end
+
   defp parseInteger(string) do
-    case Integer.parse(string)do
-      {int,_rest} -> [int]
+    case Integer.parse(string) do
+      {int, _rest} -> [int]
       :error -> []
     end
   end
 end
+
+# Tô 🌹🌹
